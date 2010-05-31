2f5e89d0ffb2f5ff87e6ff9de63f8ae1f57b1294
Jerome Schneider <jschneider@entrouvert.com>
2010-05-31 16:07:59 +0200

    Add idp application and enable admin module

--- settings.py
+++ settings.py
@@ -90,6 +90,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'authentic.idp'
 )
