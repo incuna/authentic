9add66f49ae0daa28db84e36bf2ac8675125d21c
Frédéric Péters <fpeters@0d.be>
2010-06-01 09:41:08 +0200

    Remove instructional comments

--- urls.py
+++ urls.py
@@ -4,13 +4,6 @@ from django.views.generic.simple import direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^authentic/', include('authentic.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     (r'^admin/', include(admin.site.urls)),
     (r'^registration/', include('registration.urls')),
     (r'^$', direct_to_template,
