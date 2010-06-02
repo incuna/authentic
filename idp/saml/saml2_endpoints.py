b58e591aa2a612eb8366571736481024437d73dd
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-06-02 16:07:36 +0200

    Fix rename get_metadata -> get_saml2_metadata

--- idp/saml/saml2_endpoints.py
+++ idp/saml/saml2_endpoints.py
@@ -29,7 +29,7 @@ from models import *
 
 
 def metadata(request):
-    return HttpResponse(get_metadata(request), mimetype = 'text/xml')
+    return HttpResponse(get_saml2_metadata(request), mimetype = 'text/xml')
 
 @login_required
 def sso(request):
