5d056e29c07ca771374415fd61378c2cba769d02
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-06-02 16:11:59 +0200

    Start of the ID-FFv1.2 IdP endpoints

--- idp/saml/idff12_endpoints.py
+++ idp/saml/idff12_endpoints.py
@@ -1,19 +1,27 @@
-import lasso
 from django.contrib.auth.decorators import login_required
+from django.conf.urls.defaults import *
+from django.http import *
+import lasso
+from models import *
+from authentic.saml.common import *
+import datetime
 
-def get_post_saml_request(request):
-    '''Return a SAML 1.1 request transmitted through a POST request'''
-    return request.POST.get('LAREQ')
+def metadata(request):
+    return HttpResponse(get_idff12_metadata(request), mimetype = 'text/xml')
 
 @login_required
 def sso(request):
     """Endpoint for AuthnRequests asynchronously sent, i.e. POST or Redirect"""
     # 1. Process the request, separate POST and GET treatment
-    if request.method == 'GET':
-        message = request.META.get('QUERY_STRING')
-    elif request.method == 'POST':
-        message = get_post_saml_request(request)
-
+    message = get_idff12_request_message(request)
+    if not message:
+        return HttpResponseForbidden('Invalid SAML 1.1 AuthnRequest: "%s"' % message)
+    server = create_idff12_server(request)
+    login = lasso.Login(server)
+    try:
+        login.processAuthnRequestMsg(message)
+    except lasso.ProfileInvalidMsgError:
+        return HttpResponseForbidden('Invalid SAML 1.1 AuthnRequest: "%s"' % message)
 
     # 2. Lookup the ProviderID
 
@@ -23,3 +31,8 @@ def sso(request):
 
     # 3. Depending on the requested profile finish with an artifact or a
     # POST
+
+urlpatterns = patterns('',
+    (r'^metadata/', metadata),
+    (r'^sso/', sso),
+)
--- idp/urls.py
+++ idp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 import saml.saml2_endpoints
+import saml.idff12_endpoints
 from django.conf import settings
 
 urlpatterns = patterns('',)
@@ -8,3 +9,8 @@ urlpatterns = patterns('',)
 if settings.IDP_SAML2:
     urlpatterns += patterns('',
         (r'^saml2/', include(saml.saml2_endpoints)),)
+
+if settings.IDP_IDFF12:
+    urlpatterns += patterns('',
+        (r'^idff12/', include(saml.idff12_endpoints)),)
+
