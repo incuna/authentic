a67b5d021c68caad4b9e56e7b41c1e4e54388ad8
Mikaël Ates <mates@entrouvert.com>
2010-06-02 17:47:33 +0200

    SSL Support: Admin interface for models

---
+++ sslauth/admin.py
@@ -0,0 +1,5 @@
+from authentic.sslauth.models import DistinguishedName, ClientCertificate
+from django.contrib import admin  
+   
+admin.site.register(DistinguishedName)  
+admin.site.register(ClientCertificate)
