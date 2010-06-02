201136a68a9de571415b8676dfba7c9615117a3b
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-06-02 13:17:09 +0200

    Change import name for SAML IdP in urls.py

--- idp/urls.py
+++ idp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
-import liberty.saml2_endpoints
+import saml.saml2_endpoints
 
 urlpatterns = patterns('',
     (r'^saml2/', include(saml.saml2_endpoints)),
