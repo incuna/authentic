4a4288d8383c6273ccf9b867671ed736be29a066
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-06-03 23:34:52 +0200

    SAML common: add customization to LibertyProvider admin view

--- saml/admin.py
+++ saml/admin.py
@@ -4,10 +4,22 @@ from models import *
 
 class LibertyProviderAdmin(admin.ModelAdmin):
     list_display = ('name', 'entity_id', 'protocol_conformance')
+    list_display_links = ('entity_id',)
+    list_editable = ('name',)
+    search_fields = ('name', 'entity_id')
+    readonly_fields = ('entity_id','protocol_conformance')
     fieldsets = (
-            (None, { 'fields' : ('name', ) }),
+            (None, { 'fields' : ('name', 'entity_id') }),
             (_('Metadata files'),
                 { 'fields': ('metadata', 'public_key', 'ssl_certificate', 'ca_cert_chain') }))
 
+class LibertyAttributeMappingInline(admin.TabularInline):
+    model = LibertyAttributeMapping
+
+class LibertyAttributeMapAdmin(admin.ModelAdmin):
+    inlines = [
+            LibertyAttributeMappingInline,
+    ]
 
 admin.site.register(LibertyProvider, LibertyProviderAdmin)
+admin.site.register(LibertyAttributeMap, LibertyAttributeMapAdmin)
