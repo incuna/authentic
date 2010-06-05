1e2fd4aa74ee0e9491443c37188e463dd85d40e5
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-06-05 15:09:24 +0200

    SAML idp: implement SAML listing of services on homepage

---
+++ idp/saml/__init__.py
@@ -0,0 +1,20 @@
+from authentic.idp import register_service_list
+from authentic.saml.models import LibertyServiceProvider
+import urllib
+
+def saml_service_list(request):
+    q = LibertyServiceProvider.objects.filter(enabled = True, idp_initiated_sso = True)
+    list = []
+    for service_provider in q:
+        liberty_provider = service_provider.liberty_provider
+        entity_id = liberty_provider.entity_id
+        if liberty_provider.protocol_conformance < 3:
+            protocol = 'idff12'
+        else:
+            protocol = 'saml2'
+        uri = '/idp/%s/idp_sso/%s' % (protocol, urllib.quote(entity_id))
+        name = liberty_provider.name
+        list.append((uri, name))
+    return list
+
+register_service_list(saml_service_list)
