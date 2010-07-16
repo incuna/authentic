eb60729c53700abad7b3f58db30f6eb67b7fb733
BAUDINET Bertrand <bbaudinet@entrouvert.com>
2010-07-16 14:06:12 +0200

    [openid-provider] Reject no-encryption association sessions over http
    - Reject no-encryption association sessions over http, to avoid eavesdropper
    and to meet the point 8.4.1 of the specification of OpenID 2.0

--- django_openid_provider/views.py
+++ django_openid_provider/views.py
@@ -1,13 +1,16 @@
 from django.views.decorators.csrf import csrf_exempt
 from openid_provider.views import openid_decide as origin_openid_decide
-from openid_provider.views import openid_server as origin_openid_server
-from django.http import HttpResponse
+from openid_provider.views import openid_is_authorized
+from openid_provider.views import django_response
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import get_template
 from django.template import Context
 from django.views.generic.simple import redirect_to
 from django import forms
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from openid.server.server import Server
+from django.core.urlresolvers import reverse
 import re
 from openid_provider.views import get_base_uri
 from django.template import RequestContext
@@ -15,7 +18,62 @@ import settings
 
 @csrf_exempt
 def openid_server(req):
-    return origin_openid_server(req)
+    """
+    This view is the actual OpenID server - running at the URL pointed to by 
+    the <link rel="openid.server"> tag. 
+    """
+    host = get_base_uri(req)
+    try:
+        # if we have django_openid_auth in applications directory
+        # then we can use DjangoOpenIDStore
+        from django_openid_auth.store import DjangoOpenIDStore
+        store = DjangoOpenIDStore()
+    except:
+        # otherwise use FileOpenIDStore
+        OPENID_FILESTORE = '/tmp/openid-filestore'
+        from openid.store.filestore import FileOpenIDStore
+        store = FileOpenIDStore(OPENID_FILESTORE)
+        
+    server = Server(store, op_endpoint="%s%s" % (host, reverse('openid-provider-root')))
+
+    # Clear AuthorizationInfo session var, if it is set
+    if req.session.get('AuthorizationInfo', None):
+        del req.session['AuthorizationInfo']
+
+    querydict = dict(req.REQUEST.items())
+    orequest = server.decodeRequest(querydict)
+    if not orequest:
+        orequest = req.session.get('OPENID_REQUEST', None)
+        if not orequest:
+            # not request, render info page:
+            return render_to_response('openid_provider/server.html',
+                {'host': host,},
+                context_instance=RequestContext(req))
+        else:
+            # remove session stored data:
+            del req.session['OPENID_REQUEST']
+    if orequest.mode in ("checkid_immediate", "checkid_setup"):
+
+        if not req.user.is_authenticated():
+            return landing_page(req, orequest)
+
+        openid = openid_is_authorized(req, orequest.identity, orequest.trust_root)
+
+        if openid is not None:
+            oresponse = orequest.answer(True, identity="%s%s" % (
+                host, reverse('openid-provider-identity', args=[openid.openid])))
+        elif orequest.immediate:
+            raise Exception('checkid_immediate mode not supported')
+        else:
+            req.session['OPENID_REQUEST'] = orequest
+            return HttpResponseRedirect(reverse('openid-provider-decide'))
+    else:
+        for k,v in orequest.message.args.iteritems():
+            if v == 'no-encryption' and not req.is_secure():
+                return error_page(req,"Association with no encryption over http is forbidden")
+        oresponse = server.handleRequest(orequest)
+    webresponse = server.encodeResponse(oresponse)
+    return django_response(webresponse)
 
 @csrf_exempt
 def openid_decide(req):
