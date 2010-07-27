08585455b09ad35b5f2b924ae2354457456daa2a
BAUDINET Bertrand <bbaudinet@entrouvert.com>
2010-07-27 11:45:51 +0200

    Fixes bug #178 "Log out don't redirect to the homepage"
    When the user logout he is redirect to the homepage

--- idp/views.py
+++ idp/views.py
@@ -34,7 +34,7 @@ def authsaml2_login_page(request):
 
 def AuthLogout(request, next_page=None, redirect_field_name=REDIRECT_FIELD_NAME):
     auth_logout.send(sender = None, user = request.user)
-    return logout(request, template_name = 'registration/logout.html', next_page = next_page, redirect_field_name=redirect_field_name)
+    return logout(request, template_name = 'registration/logout.html', next_page = '/', redirect_field_name=redirect_field_name)
 
 
 @csrf_exempt
