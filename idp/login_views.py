27d983505906db113b6f8cd700b971c8ec1020a3
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-10-06 19:47:13 +0200

    [idp] make nonce field optional in login form

--- idp/login_views.py
+++ idp/login_views.py
@@ -4,24 +4,23 @@ from django import forms
 import models
 
 class WithNonceAuthenticationForm(AuthenticationForm):
-    nonce = forms.CharField(label='Nonce', widget=forms.HiddenInput)
+    nonce = forms.CharField(label='Nonce', widget=forms.HiddenInput,
+            required = False)
 
     def __init__(self, request = None, **kwargs):
         AuthenticationForm.__init__(self, request, **kwargs)
         if request and request.REQUEST.has_key('nonce'):
-            print 'setting nonce to', request.REQUEST.get('nonce')
             self.initial['nonce'] = request.REQUEST.get('nonce')
-            print self
 
     def clean(self):
         res = AuthenticationForm.clean(self)
         # create an authentication event
         if self.cleaned_data.has_key('nonce'):
             how = 'password'
-            if self.request.environ.has_key('HTTPS'):
+            if self.request and self.request.environ.has_key('HTTPS'):
                 how = 'password-on-https'
             models.AuthenticationEvent(who = self.user_cache.username,
-                    how = how, nonce = self['nonce']).save()
+                    how = how, nonce = self.cleaned_data['nonce']).save()
         return res
 
 def login(request):
