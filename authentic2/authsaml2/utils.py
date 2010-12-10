039a694121618bddab0c5a8e90f49649a846ebda
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-12-10 19:54:37 +0100

    [authsaml2] remove unused import

--- authentic2/authsaml2/utils.py
+++ authentic2/authsaml2/utils.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from django.shortcuts import render_to_response
 from django.utils.translation import ugettext as _
 
 import logging
