0cc292f19e853e3d83087b5b787c44d2d8f94c17
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-12-21 12:14:05 +0100

    [authsaml2] remove the context processor

--- authentic2/authsaml2/context_processor.py
+++
@@ -1,7 +0,0 @@
-import authentic2.authsaml2
-import authentic2.saml.common
-
-def authsaml2_login_page(request):
-    if not authentic2.authsaml2.utils.is_sp_configured():
-        return {}
-    return {'providers_list': authentic2.saml.common.get_idp_list()}
