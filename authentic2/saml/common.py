43145b9d585d76411472796e3ec087829bd23229
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-12-22 16:55:19 +0100

    [saml common] make get_http_binding more resilient
    Form posting is usually more standard and does not use a bad content
    type. On the other hand SOAP is usually badly implemented, so we prefer
    to consider a POST as a SOAP message if the content type is not the one
    used for an HTML form.

--- authentic2/saml/common.py
+++ authentic2/saml/common.py
@@ -47,10 +47,11 @@ def get_http_binding(request):
         return 'GET'
     elif request.method == 'POST':
         # disambiguate SOAP and form POST
-        if request.META.get('CONTENT_TYPE') == 'text/xml':
-            return 'SOAP'
-        else:
+        if request.META['CONTENT_TYPE'] in
+            ('application/x-www-form-urlencoded', 'multipart/form-data'):
             return 'POST'
+        else:
+            return 'SOAP'
 
 # SAMLv2 methods
 
