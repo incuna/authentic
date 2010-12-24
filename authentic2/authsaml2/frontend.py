71bafc3ef7ff52ae285cf42fe7fa03175af2f7ce
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-12-24 11:31:33 +0100

    [authsaml2] cache the idp_list inside the frontend object to limit SQL requests

--- authentic2/authsaml2/frontend.py
+++ authentic2/authsaml2/frontend.py
@@ -1,4 +1,5 @@
 import urllib
+import functools
 
 from django.utils.translation import gettext_noop
 import django.forms as forms
@@ -8,16 +9,20 @@ import authentic2.saml.common as saml_common
 
 class AuthSAML2Form(forms.Form):
     def __init__(self, *args, **kwargs):
+        idp_list = kwargs.pop('idp_list')
         super(AuthSAML2Form, self).__init__(*args, **kwargs)
         self.fields['provider_id'].choices = \
-                [(p.entity_id, p.name) for p in saml_common.get_idp_list()]
+                [(p['entity_id'], p['name']) for p in idp_list]
 
     provider_id = forms.ChoiceField(label='Choose your identity provider',
             choices=())
 
 class AuthSAML2Frontend(object):
+    def __init__(self):
+        self.idp_list = saml_common.get_idp_list_sorted()
+
     def enabled(self):
-        return bool(saml_common.get_idp_list())
+        return bool(self.idp_list)
 
     def id(self):
         return 'saml2'
@@ -26,7 +31,7 @@ class AuthSAML2Frontend(object):
         return gettext_noop('SAML 2.0')
 
     def form(self):
-        return AuthSAML2Form
+        return functools.partial(AuthSAML2Form, idp_list=self.idp_list)
 
     def post(self, request, form, nonce, next):
         provider_id = form.cleaned_data['provider_id']
