609b273934636912097950c3856a23aaf00532d4
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-12-24 15:54:05 +0100

    [authsaml2] add model to represent user authenticated through a transient NameID

--- authentic2/authsaml2/models.py
+++ authentic2/authsaml2/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext as _
-from authentic2.saml.models import *
+from django.db.models.manager import EmptyManager
 import lasso
+
+from authentic2.saml.models import *
 
 AUTHSAML2_UNAUTH_PERSISTENT = (
     ('AUTHSAML2_UNAUTH_PERSISTENT_ACCOUNT_LINKING_BY_AUTH', _('Account linking by authentication')),
@@ -102,3 +104,84 @@ class ExtendDjangoSession(models.Model):
     federation_in_progress = models.CharField(max_length = 80, )
     next = models.CharField(max_length = 80, )
     temp_identity_dump = models.TextField(blank = True)
+
+
+
+class SAML2TransientUser(object):
+    '''Class compatible with django.contrib.auth.models.User which represent an
+       user authenticated using a Transien federation'''
+    id = None
+    is_staff = False
+    is_active = False
+    is_superuser = False
+    _groups = EmptyManager()
+    _user_permissions = EmptyManager()
+
+    def __init__(self, id):
+        self.id = id
+
+    def __unicode__(self):
+        return 'AnonymousUser'
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return 1 # instances always return the same hash value
+
+    def save(self):
+        pass
+
+    def delete(self):
+        raise NotImplementedError
+
+    def set_password(self, raw_password):
+        raise NotImplementedError
+
+    def check_password(self, raw_password):
+        raise NotImplementedError
+
+    def _get_groups(self):
+        return self._groups
+    groups = property(_get_groups)
+
+    def _get_user_permissions(self):
+        return self._user_permissions
+    user_permissions = property(_get_user_permissions)
+
+    def get_group_permissions(self, obj=None):
+        return set()
+
+    def get_all_permissions(self, obj=None):
+        return _user_get_all_permissions(self, obj=obj)
+
+    def has_perm(self, perm, obj=None):
+        return _user_has_perm(self, perm, obj=obj)
+
+    def has_perms(self, perm_list, obj=None):
+        for perm in perm_list:
+            if not self.has_perm(perm, obj):
+                return False
+        return True
+
+    def has_module_perms(self, module):
+        return _user_has_module_perms(self, module)
+
+    def get_and_delete_messages(self):
+        return []
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+
+    def get_username(self):
+        return _('Anonymous')
+    username = property(get_username)
