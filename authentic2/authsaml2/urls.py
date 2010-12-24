840ff15b590d728b47121b3ef683fc186627d113
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-12-24 11:36:11 +0100

    [authsaml2] rectify url dispatch file

--- authentic2/authsaml2/urls.py
+++ authentic2/authsaml2/urls.py
@@ -5,24 +5,24 @@ from saml2_endpoints import *
 urlpatterns = patterns('',
     (r'^metadata$', metadata),
     # Receive request from user interface
-    (r'^sso/(?P<entity_id>[a-zA-Z0-9\:\-\./]+)/$', sso),
-    (r'^finish_federation', finish_federation),
-    (r'^singleSignOnArtifact', singleSignOnArtifact),
-    (r'^singleSignOnPost', singleSignOnPost),
+    (r'^sso$', sso),
+    (r'^finish_federation$', finish_federation),
+    (r'^singleSignOnArtifact$', singleSignOnArtifact),
+    (r'^singleSignOnPost$', singleSignOnPost),
     # Receive request from user interface
-    (r'^logout', logout),
+    (r'^logout$', logout),
     # Receive response from Redirect SP initiated
-    (r'^singleLogoutReturn', singleLogoutReturn),
+    (r'^singleLogoutReturn$', singleLogoutReturn),
     # Receive request from SOAP IdP initiated
-    (r'^singleLogoutSOAP', singleLogoutSOAP),
+    (r'^singleLogoutSOAP$', singleLogoutSOAP),
     # Receive request from Redirect IdP initiated
-    (r'^singleLogout', singleLogout),
+    (r'^singleLogout$', singleLogout),
     # Receive request from user interface
     (r'^federationTermination/(?P<entity_id>[a-zA-Z0-9\:\-\./]+)/$', federationTermination),
     # Receive response from Redirect SP initiated
-    (r'^manageNameIdReturn', manageNameIdReturn),
+    (r'^manageNameIdReturn$', manageNameIdReturn),
     # Receive request from SOAP IdP initiated
-    (r'^manageNameIdSOAP', manageNameIdSOAP),
+    (r'^manageNameIdSOAP$', manageNameIdSOAP),
     # Receive request from Redirect IdP initiated
-    (r'^manageNameId', manageNameId),
+    (r'^manageNameId$', manageNameId),
 )
