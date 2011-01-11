79832289c17bc450cdc3cefe74b193cf29e0138a
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-01-11 01:08:37 +0100

    [idp openid] remove OpenID model, anchor TrustedRoot to user id, not user object

--- authentic2/idp/openid/models.py
+++ authentic2/idp/openid/models.py
@@ -13,36 +13,10 @@ from django.contrib.auth.models import User
 
 from authentic2.saml.fields import PickledObjectField
 
-class OpenID(models.Model):
-    user = models.ForeignKey(User)
-    openid = models.CharField(max_length=200, blank=True, unique=True)
-    default = models.BooleanField(default=False)
-
-    class Meta:
-        verbose_name = _('OpenID')
-        verbose_name_plural = _('OpenIDs')
-        ordering = ['openid']
-
-    def __unicode__(self):
-        return u"%s|%s" % (self.user.username, self.openid)
-
-    def save(self, *args, **kwargs):
-        if self.openid in ['', u'', None]:
-            from hashlib import sha1
-            import random, base64
-            sha = sha1()
-            sha.update(unicode(self.user.username).encode('utf-8'))
-            sha.update(str(random.random()))
-            value = str(base64.b64encode(sha.digest()))
-            value = value.replace('/', '').replace('+', '').replace('=', '')
-            self.openid = value
-        super(OpenID, self).save(*args, **kwargs)
-        if self.default:
-            self.user.openid_set.exclude(pk=self.pk).update(default=False)
-
 class TrustedRoot(models.Model):
-    openid = models.ForeignKey(OpenID)
+    user = models.CharField(max_length=255)
     trust_root = models.CharField(max_length=200)
+    choices = PickledObjectField()
 
     def __unicode__(self):
         return unicode(self.trust_root)
