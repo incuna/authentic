4d0b6bfbda134d6ab08c79825bd9d5e25d137f2c
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-01-11 01:10:40 +0100

    [idp openid] simplify openid provider, only support directed identity, add attribute selection to consent page

--- authentic2/idp/openid/views.py
+++ authentic2/idp/openid/views.py
@@ -3,6 +3,9 @@
 # some code from http://www.djangosnippets.org/snippets/310/ by simon
 # and from examples/djopenid from python-openid-2.2.4
 
+import logging
+import hashlib
+
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
@@ -14,6 +17,7 @@ try:
     from django.views.decorators.csrf import csrf_exempt
 except ImportError:
     from django.contrib.csrf.middleware import csrf_exempt
+import django.forms as forms
 
 from django.contrib.auth import REDIRECT_FIELD_NAME
 
@@ -23,8 +27,14 @@ from openid.server.server import Server
 from openid.server.trustroot import verifyReturnTo
 from openid.yadis.discover import DiscoveryFailure
 from openid.yadis.constants import YADIS_CONTENT_TYPE
+from openid.message import IDENTIFIER_SELECT
+from openid.extensions.sreg import ns_uri as SREG_TYPE, SRegRequest, \
+        SRegResponse, data_fields
 
-from utils import add_sreg_data, get_store
+from utils import add_sreg_data, get_store, oresponse_to_response
+from authentic2.auth.views import redirect_to_login
+
+logger = logging.getLogger('authentic2.idp.openid')
 
 @csrf_exempt
 def openid_server(request):
@@ -47,51 +57,79 @@ def openid_server(request):
             # remove session stored data:
             del request.session['OPENID_REQUEST']
         else:
-            # not request, render info page:
-            return render_to_response('openid_provider/server.html', {
-                'host': request.build_absolute_uri('/'),
-                'xrds_location': request.build_absolute_uri(
-                    reverse('openid-provider-xrds')),
-            }, context_instance=RequestContext(request))
+            return HttpResponseRedirect('/')
 
     if orequest.mode in ("checkid_immediate", "checkid_setup"):
+        # User refused
+        if 'cancel' in request.REQUEST:
+            return utils.oresponse_to_response(server, orequest.answer(False))
+        # User is not logged
         if not request.user.is_authenticated():
-            return landing_page(request, orequest)
-
-        openid = openid_is_authorized(request, orequest.identity, orequest.trust_root)
-
-        if openid is not None:
-            id_url = request.build_absolute_uri(
-                reverse('openid-provider-identity', args=[openid.openid]))
-            oresponse = orequest.answer(True, identity=id_url)
-        elif orequest.immediate:
-            raise Exception('checkid_immediate mode not supported')
+            # Site does not want interaction
+            if orequest.immediate:
+                return utils.oresponse_to_response(server, orequest.answer(False))
+            else:
+            # Try to login
+                request.session['OPENID_REQUEST'] = orequest
+                return redirect_to_login(request, nonce='1')
         else:
-            request.session['OPENID_REQUEST'] = orequest
-            return HttpResponseRedirect(reverse('openid-provider-decide'))
+            identity = orequest.identity
+            if identity != IDENTIFIER_SELECT:
+                # We only support directed identity
+                return utils.oresponse_to_response(server, orequest.answer(False))
+
+            try:
+                trusted_root = models.TrustedRoot.objects.get(user=request.user.id,
+                        trust_root=orequest.trust_root)
+                user_data = {}
+                for field in trusted_root.choices:
+                    if field == 'email':
+                        user_data[field] = request.user.email
+                    elif field == 'fullname':
+                        user_data[field] = '%s %s' % (request.user.first_name,
+                                request.user.last_name)
+                    else:
+                        logger.warning('Could not provider SReg field %s' % field)
+            except models.TrustedRoot.DoesNotExit:
+                # RP does not want any interaction
+                if orequest.immediate:
+                    return utils.oresponse_to_response(server, orequest.answer(False))
+                request.session['OPENID_REQUEST'] = orequest
+                return HttpResponseRedirect(reverse('openid-provider-decide'))
+
+        # Create a directed identity
+        hash = hashlib.sha1(request.user.id+'|'+orequest.trust_root).hexdigest()
+        claimed_id = request.build_absolute_uri(
+                reverse('openid-provider-identity', args=[hash]))
+
+        oresponse = orequest.answer(True, identity=orequest.identite,
+                claimed_id=claimed_id)
+        sreg_request = SRegRequest.fromOpenIDRequest(orequest)
+        sreg_response = SRegResponse.extractResponse(sreg_request, user_data)
+        oresponse.addExtension(sreg_response)
     else:
         oresponse = server.handleRequest(orequest)
-    if request.user.is_authenticated():
-        add_sreg_data(request, orequest, oresponse)
-    # Convert a webresponse from the OpenID library in to a Django HttpResponse
-    webresponse = server.encodeResponse(oresponse)
-    response = HttpResponse(webresponse.body)
-    response.status_code = webresponse.code
-    for key, value in webresponse.headers.items():
-        response[key] = value
-    return response
+    return utils.oresponse_to_response(server, oresponse)
 
 def openid_xrds(request, identity=False, id=None):
     if identity:
         types = [OPENID_2_0_TYPE]
     else:
-        types = [OPENID_IDP_2_0_TYPE]
+        types = [OPENID_IDP_2_0_TYPE,SREG_TYPE]
     endpoints = [request.build_absolute_uri(reverse('openid-provider-root'))]
     return render_to_response('openid_provider/xrds.xml', {
         'host': request.build_absolute_uri('/'),
         'types': types,
         'endpoints': endpoints,
     }, context_instance=RequestContext(request), mimetype=YADIS_CONTENT_TYPE)
+
+
+class DecideForm(forms.Form):
+    def __init__(self, sreg_request=[], *args, **kwargs):
+        super(DecideForm, self).__init__(*args, **kwargs)
+        for field in self.optional:
+            self.fields[field] = forms.BooleanField(label=data_fields[field])
+
 
 def openid_decide(request):
     """
@@ -101,17 +139,28 @@ def openid_decide(request):
     # If they are NOT logged in, show the landing page
     """
     orequest = request.session.get('OPENID_REQUEST')
-
+    # No request ? Failure..
+    if not orequest:
+        return HttpResponseRedirect('/')
+    sreg_request = SRegRequest.fromOpenIDRequest(orequest)
     if not request.user.is_authenticated():
-        return landing_page(request, orequest)
+        # Not authenticated ? Authenticate and go back to the server endpoint
+        return redirect_to_login(request, next=reverse(openid_server), nonce='1')
 
-    openid = openid_get_identity(request, orequest.identity)
-    if openid is None:
-        return error_page(request, "You are signed in but you don't have OpenID here!")
-
-    if request.method == 'POST' and request.POST.get('decide_page', False):
-        openid.trustedroot_set.create(trust_root=orequest.trust_root)
-        return HttpResponseRedirect(reverse('openid-provider-root'))
+    if request.method == 'POST':
+        if 'cancel' in request.POST:
+            # User refused
+            return HttpResponseRedirect('%s?cancel' % reverse(openid_server))
+        else:
+            form = DecideForm(sreg_request=sreg_request,data=request.POST)
+            if form.is_valid():
+                data = form.cleaned_data()
+                # Remember the choice
+                models.TrustedRoot(user=request.user.id,
+                        trust_root=orequest.trust_root,
+                        choices=sreg_request.required +
+                    [ field for field in data if data[field] ])
+                return HttpResponseRedirect(reverse('openid-provider-root'))
 
     # verify return_to of trust_root
     try:
@@ -121,64 +170,9 @@ def openid_decide(request):
     except DiscoveryFailure:
         trust_root_valid = "DISCOVERY_FAILED"
 
-    return render_to_response('openid_provider/decide.html', {
+    return render_to_response('idp/openid/decide.html', {
         'title': _('Trust this site?'),
-        'trust_root': orequest.trust_root,
+        'required': sreg_request.required,
         'trust_root_valid': trust_root_valid,
-        'identity': orequest.identity,
+        'form': form,
     }, context_instance=RequestContext(request))
-
-def error_page(request, msg):
-    return render_to_response('error.html', {
-        'title': _('Error'),
-        'msg': msg,
-    }, context_instance=RequestContext(request))
-
-def landing_page(request, orequest):
-    """
-    The page shown when the user attempts to sign in somewhere using OpenID 
-    but is not authenticated with the site. For idproxy.net, a message telling
-    them to log in manually is displayed.
-    """
-    request.session['OPENID_REQUEST'] = orequest
-    login_url = settings.LOGIN_URL
-    path = request.get_full_path()
-    url = '%s?%s=%s' % (login_url, REDIRECT_FIELD_NAME, urlquote(path))
-    return HttpResponseRedirect(url)
-
-def openid_is_authorized(request, identity_url, trust_root):
-    """
-    Check that they own the given identity URL, and that the trust_root is 
-    in their whitelist of trusted sites.
-    """
-    if not request.user.is_authenticated():
-        return None
-
-    openid = openid_get_identity(request, identity_url)
-    if openid is None:
-        return None
-
-    if openid.trustedroot_set.filter(trust_root=trust_root).count() < 1:
-        return None
-
-    return openid
-
-def openid_get_identity(request, identity_url):
-    """
-    Select openid based on claim (identity_url).
-    If none was claimed identity_url will be 'http://specs.openid.net/auth/2.0/identifier_select'
-    - in that case return default one
-    - if user has no default one, return any
-    - in other case return None!
-    """
-    for openid in request.user.openid_set.iterator():
-        if identity_url == request.build_absolute_uri(
-                reverse('openid-provider-identity', args=[openid.openid])):
-            return openid
-    if identity_url == 'http://specs.openid.net/auth/2.0/identifier_select':
-        # no claim was made, choose user default openid:
-        openids = request.user.openid_set.filter(default=True)
-        if openids.count() == 1:
-            return openids[0]
-        return request.user.openid_set.all()[0]
-    return None
