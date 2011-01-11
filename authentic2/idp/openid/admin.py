2ae8f4f7505f77670e8c5e4f20ab3fcbf49ebd3c
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-01-11 01:08:03 +0100

    [idp openid] update models registration in admin site

--- authentic2/idp/openid/admin.py
+++ authentic2/idp/openid/admin.py
@@ -2,13 +2,8 @@
 # vim: set ts=4 sw=4 : */
 
 from django.contrib import admin
-from models import TrustedRoot, OpenID
+from models import TrustedRoot, Association, Nonce
 
-class TrustedRootInline(admin.TabularInline):
-    model = TrustedRoot
-
-class OpenIDAdmin(admin.ModelAdmin):
-    list_display = ['openid', 'user', 'default']
-    inlines = [TrustedRootInline, ]
-
-admin.site.register(OpenID, OpenIDAdmin)
+admin.site.register(TrustedRoot)
+admin.site.register(Association)
+admin.site.register(Nonce)
