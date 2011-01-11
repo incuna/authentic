c5e07a666611c14d3372f113f14e4a72b2ff9841
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-01-11 01:11:14 +0100

    [urls] include urls patterns from the idp.openid module

--- authentic2/urls.py
+++ authentic2/urls.py
@@ -52,19 +52,9 @@ if settings.STATIC_SERVE:
             kwargs = {'document_root': settings.MEDIA_ROOT}),
     )
 
-if settings.IDP_OPENID:
-    import authentic2.django_openid_provider.views
-    import openid_provider.views
+if getattr(settings, 'IDP_OPENID', False):
     urlpatterns += patterns('',
-            (r'^openid/$',authentic2.django_openid_provider.views.openid_server, {},'openid-provider-root'),
-            (r'^openid/decide/$',authentic2.django_openid_provider.views.openid_decide, {},'openid-provider-decide'),
-            (r'^openid/manage/',authentic2.django_openid_provider.views.manage_trustroot, {},'manage_trustroot'),
-            (r'^openid/manageid/',authentic2.django_openid_provider.views.manage_id, {},'manage_id' ),
-            (r'^openid/manageid_confirm/',authentic2.django_openid_provider.views.manage_id_confirm, {}, 'manage_id_confirm'),
-            (r'^openid/addopenid/',authentic2.django_openid_provider.views.addopenid, {}, 'add_openid'),
-            (r'^openid/',include('openid_provider.urls')),
-            url(r'^o8/(?P<id>[a-zA-Z0-9,_,]*/?)/$', openid_provider.views.openid_xrds, {'identity': True}, name='openid-provider-identity'),
-    )
+            (r'^openid/', include('authentic2.idp.openid.urls')))
 
 if 'authentic2.auth.oath' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
