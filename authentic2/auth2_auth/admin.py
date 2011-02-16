f14b5983fe6f64322f6a16d6b22a6ac1fa414f4a
Mikaël Ates <mates@entrouvert.com>
2011-02-16 11:43:41 +0100

    [auth2_auth] Bugfix

--- authentic2/auth2_auth/admin.py
+++ authentic2/auth2_auth/admin.py
@@ -5,6 +5,6 @@ from authentic2.auth2_auth.models import *
 class AuthenticationEventAdmin(admin.ModelAdmin):
     date_hierarchy = 'when'
 
-if settings.DEBUG:
-    admin.site.register(AuthenticationEvent, AuthenticationEventAdmin)
+#if settings.DEBUG:
+#    admin.site.register(AuthenticationEvent, AuthenticationEventAdmin)
 
