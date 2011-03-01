912a55e77a45abd5d23c975c207ea91882276caa
Mikaël Ates <mates@entrouvert.com>
2011-03-01 16:01:54 +0100

    [idp][saml] Display attributes to the consent form

--- authentic2/idp/saml/interaction.py
+++ authentic2/idp/saml/interaction.py
@@ -9,8 +9,41 @@ def consent(request, nonce = '', next = None, provider_id = None):
 
        On a POST handle the form and redirect to next'''
     if request.method == "GET":
+        attributes = []
+        import sys
+        print >> sys.stderr, "request.session.has_key('attributes_to_send') " + str(request.session['attributes_to_send'])
+        if request.session.has_key('attributes_to_send'):
+            attrs = request.session['attributes_to_send']
+            for key in attrs:
+                name = None
+                if type(key) is tuple and len(key) == 2:
+                    name, format = key
+                elif type(key) is tuple:
+                    return
+                else:
+                    name = key
+                v = name + ' '
+                print >> sys.stderr, "v " + v
+                values = attrs[key]
+                for value in values:
+                    if value is True:
+                        value = 'true'
+                    elif value is False:
+                        value = 'false'
+                    else:
+                        value = str(value)
+                    if type(value) is unicode:
+                        value = value.encode('utf-8')
+                    #else:
+                    #    value = sitecharset2utf8(value)
+                    v += value + ' '
+                print >> sys.stderr, "v " + v
+                attributes.append(v)
+                print >> sys.stderr, "attributes " + str(attributes)
+
         return render_to_response('interaction/consent.html',
             {'provider_id': request.GET.get('provider_id', ''),
+             'attributes': attributes,
              'nonce': request.GET.get('nonce', ''),
              'next': request.GET.get('next', '')},
             context_instance=RequestContext(request))
