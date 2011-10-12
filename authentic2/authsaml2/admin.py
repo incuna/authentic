4bd1b3910c866743901ba275540d32cce2a54607
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-10-12 14:14:46 +0200

    [authsaml2] cleanup admin and urls module
     * remove `import *'
     * add explicit import of MyServiceProvider
     * add explicit import of patterns from django.conf.urls.defaults
     * add explicit imports of view methods from saml2_endpoints

--- authentic2/authsaml2/admin.py
+++ authentic2/authsaml2/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from models import *
-from django.utils.translation import ugettext as _
+from models import MyServiceProvider
 
 class MyServiceProviderAdmin(admin.ModelAdmin):
     fieldsets = (
