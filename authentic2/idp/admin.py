185fbf47bdf49bc4e29fee29d6545812a681e6af
Mikaël Ates <mates@entrouvert.com>
2011-10-12 12:12:46 +0200

    [idp] Add new models to model administration interface

--- authentic2/idp/admin.py
+++ authentic2/idp/admin.py
@@ -2,5 +2,49 @@ from django.contrib import admin
 from models import UserConsentAttributes
 from django.conf import settings
 
+from models import AttributeItem, AttributeList, AttributePolicy
+from authentic2.attribute_aggregator.models import AttributeSource, \
+    LdapSource, UserAliasInSource
+
+
+class AttributeListAdmin(admin.ModelAdmin):
+    filter_horizontal = ('attributes', )
+    fieldsets = (
+            (None, {
+                'fields' : (
+                    'name',
+                    'attributes',
+                )
+            }),
+    )
+
+
+class AttributePolicyAdmin(admin.ModelAdmin):
+    filter_horizontal = ('source_filter_for_sso_from_push_sources', )
+    fieldsets = (
+            (None, {
+                'fields' : (
+                    'name',
+                    'attribute_list_for_sso_from_pull_sources',
+                    'forward_attributes_from_push_sources',
+                    'map_attributes_from_push_sources',
+                    'output_name_format',
+                    'output_namespace',
+                    'source_filter_for_sso_from_push_sources',
+                    'attribute_filter_for_sso_from_push_sources',
+                    'filter_source_of_filtered_attributes',
+                    'map_attributes_of_filtered_attributes',
+                    'send_error_and_no_attrs_if_missing_required_attrs',
+                )
+            }),
+    )
+
+admin.site.register(AttributeItem)
+admin.site.register(AttributeList, AttributeListAdmin)
+admin.site.register(AttributePolicy, AttributePolicyAdmin)
+admin.site.register(AttributeSource)
+admin.site.register(LdapSource)
+admin.site.register(UserAliasInSource)
+
 if settings.DEBUG:
     admin.site.register(UserConsentAttributes)
