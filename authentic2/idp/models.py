99df51a15dd7abda1b4a3f43e45b0d39b4139988
Mikaël Ates <mates@entrouvert.com>
2011-12-20 14:44:13 +0100

    [idp] Add missing verbose names of model fields.

--- authentic2/idp/models.py
+++ authentic2/idp/models.py
@@ -67,7 +67,7 @@ class UserProfile(models.Model):
             blank=True)
     email = models.EmailField(_('e-mail address'), blank=True)
     nickname = models.CharField(_('nickname'), max_length=50, blank=True)
-    url = models.URLField("Website", blank=True)
+    url = models.URLField(_('Website'), blank=True)
     company = models.CharField(verbose_name=_("Company"),
             max_length=50, blank=True)
     phone = models.CharField(verbose_name=_("Phone"),
@@ -90,18 +90,28 @@ class UserProfile(models.Model):
 
 
 class AttributeItem(models.Model):
-    attribute_name = models.CharField(max_length = 100, choices = ATTRIBUTES,
+    attribute_name = models.CharField(
+        verbose_name = _("Attribute name"),
+        max_length = 100, choices = ATTRIBUTES,
         default = ATTRIBUTES[0])
     # ATTRIBUTE_VALUE_FORMATS[0] =>
     #    (lasso.SAML2_ATTRIBUTE_NAME_FORMAT_BASIC, 'SAMLv2 BASIC')
-    output_name_format = models.CharField(max_length = 100,
+    output_name_format = models.CharField(
+        verbose_name = _("Output name format"),
+        max_length = 100,
         choices = ATTRIBUTE_VALUE_FORMATS,
         default = ATTRIBUTE_VALUE_FORMATS[0])
     #ATTRIBUTES_NS[0] => ('Default', 'Default')
-    output_namespace = models.CharField(max_length = 100,
+    output_namespace = models.CharField(
+        verbose_name = _("Output namespace"),
+        max_length = 100,
         choices = ATTRIBUTES_NS, default = ATTRIBUTES_NS[0])
-    required = models.BooleanField(default=False)
-    source = models.ForeignKey(AttributeSource, blank = True, null = True)
+    required = models.BooleanField(
+        verbose_name = _("Required"),
+        default=False)
+    source = models.ForeignKey(AttributeSource,
+        verbose_name = _("Attribute source"),
+        blank = True, null = True)
 
     class Meta:
         verbose_name = _('attribute of a list (SSO Login)')
@@ -117,8 +127,11 @@ class AttributeItem(models.Model):
 
 
 class AttributeList(models.Model):
-    name = models.CharField(max_length = 100, unique = True)
+    name = models.CharField(
+        verbose_name = _("Name"),
+        max_length = 100, unique = True)
     attributes = models.ManyToManyField(AttributeItem,
+        verbose_name = _("Attributes"),
         related_name = "attributes of the list",
         blank = True, null = True)
 
@@ -144,6 +157,7 @@ class AttributePolicy(models.Model):
     # The output format and namespace is given by each attribute.
     attribute_list_for_sso_from_pull_sources = \
         models.ForeignKey(AttributeList,
+        verbose_name = _("Pull attributes list"),
         related_name = "attributes from pull sources",
         blank = True, null = True)
 
@@ -151,7 +165,9 @@ class AttributePolicy(models.Model):
     # Attributes are in session.
     # All attributes are forwarded as is except if the parameter
     # 'map_attributes_from_push_sources' is initialized
-    forward_attributes_from_push_sources = models.BooleanField(default=False)
+    forward_attributes_from_push_sources = models.BooleanField(
+        verbose_name = _("Forward pushed attributes"),
+        default=False)
 
     # Map attributes in session
     # forward_attributes_in_session must be true
@@ -159,44 +175,61 @@ class AttributePolicy(models.Model):
     # At true, look for the namespace of the source for input, If not found,
     # system namespace. Look for the options attribute_name_format and
     # output_namespace of the attribute policy for output.
-    map_attributes_from_push_sources = models.BooleanField(default=False)
+    map_attributes_from_push_sources = models.BooleanField(
+        verbose_name = _("Map forwarded pushed attributes"),
+        default=False)
 
     # ATTRIBUTE_VALUE_FORMATS[0] =>
     #    (lasso.SAML2_ATTRIBUTE_NAME_FORMAT_BASIC, 'SAMLv2 BASIC')
-    output_name_format = models.CharField(max_length = 100,
+    output_name_format = models.CharField(
+        verbose_name = _("Output name format"),
+        max_length = 100,
         choices = ATTRIBUTE_VALUE_FORMATS,
         default = ATTRIBUTE_VALUE_FORMATS[0])
 
     #ATTRIBUTES_NS[0] => ('Default', 'Default')
-    output_namespace = models.CharField(max_length = 100,
+    output_namespace = models.CharField(
+        verbose_name = _("Output namespace"),
+        max_length = 100,
         choices = ATTRIBUTES_NS, default = ATTRIBUTES_NS[0])
 
     # Filter attributes pushed from source.
     source_filter_for_sso_from_push_sources = \
         models.ManyToManyField(AttributeSource,
+        verbose_name = \
+            _("Filter by source the forwarded pushed attributes"),
         related_name = "filter attributes of push sources with sources",
         blank = True, null = True)
 
-    # List of attributes to filter from pull sources at SSO Login.
+    # List of attributes to filter from push sources at SSO Login.
     attribute_filter_for_sso_from_push_sources = \
         models.ForeignKey(AttributeList,
+        verbose_name = \
+            _("Filter by attribute names the forwarded pushed attributes"),
         related_name = "filter attributes of push sources with list",
         blank = True, null = True)
 
     # The sources of attributes of the previous list are considered.
     # May be used conjointly with 'source_filter_for_sso_from_push_sources'
-    filter_source_of_filtered_attributes = models.BooleanField(default=False)
+    filter_source_of_filtered_attributes = models.BooleanField(
+        verbose_name = \
+    _("Filter by source and per attribute the forwarded pushed attributes"),
+        default=False)
 
     # To map the attributes of forwarded attributes with the defaut output
     # format and namespace, use 'map_attributes_from_pull_sources'
     # Use the following option to use the output format and namespace
     # indicated for each attribute.
-    map_attributes_of_filtered_attributes = models.BooleanField(default=False)
-
+    map_attributes_of_filtered_attributes = models.BooleanField(
+        verbose_name = _("Map filtered attributes"),
+        default=False)
 
     # Set to true to take in account missing required attributes
     send_error_and_no_attrs_if_missing_required_attrs = \
-        models.BooleanField(default=False)
+        models.BooleanField(
+        verbose_name = \
+            _("Send an error when a required attribute is missing"),
+        default=False)
 
     # Ask user consent
     #ask_user_consent = models.BooleanField(default=False)
