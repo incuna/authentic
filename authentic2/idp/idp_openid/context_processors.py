d6929fa152bb8149fa9b4033135441030dd71260
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-04-18 10:37:21 +0200

    Remove dependency on openid in the base template (bis)
    Fixes #1357

--- authentic2/idp/idp_openid/context_processors.py
+++ authentic2/idp/idp_openid/context_processors.py
@@ -1,11 +1,13 @@
+from django.core.urlresolvers import reverse
+
 def get_url():
     return reverse('openid-provider-xrds')
 
 def openid_meta(request):
     context = {
-        'openid_server': context['request'].build_absolute_uri(get_url())
+        'openid_server': request.build_absolute_uri(get_url())
     }
     content = '''<meta http-equiv="X-XRDS-Location" content="%(openid_server)s"/>
     <meta http-equiv="X-YADIS-Location" content="%(openid_server)s" />
 ''' % context
-    return { 'openid_meta': context }
+    return { 'openid_meta': content }
